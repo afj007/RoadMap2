fcc707421aa94e3ccf25eaaa43a49b3d607df9c5
afj007 <afj033.aj@gmail.com>
2018-06-25 18:32:26 -0300

    Arrumando a Classe SelectionSort

--- src/Ordenacao/BubbleSort.java
+++ src/Ordenacao/BubbleSort.java
@@ -6,18 +6,22 @@ public class BubbleSort {
 
 //    private static int lista[] = {100, 40, 6, 25, 78, 18, 93, 110, 55, 23};
 
-    static void ordernarValores(int lista[]) {
-        int menor = 0;
-        for (int i = 0; i < lista.length; i++){
+    static void ordernarValores(int lista[], int totalDeLoops) {
+        int anterior;
 
-            for (int j = 1; j < lista.length; i++){
-                if(j == 0){
-                    menor = lista[j];
-                }
-                if(menor)
+        for (int i = 0; i < lista.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+
+            if (lista[i - 1] > lista[i]) {
+                anterior = lista[i - 1];
+                lista[i - 1] = lista[i];
+                lista[i] = anterior;
+                i = 0;
             }
         }
-
+        mostrarValores(lista);
     }
 
     private static void mostrarValores(int lista[]) {
